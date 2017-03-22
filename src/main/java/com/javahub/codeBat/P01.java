949e77989bab573ac7418373b14eac8f73ab831f
Rajivikram <rajifr4@gmail.com>
2017-03-22 16:10:34 +0530

    Refractor is done

---
+++ src/main/java/com/javahub/codeBat/P01.java
@@ -0,0 +1,19 @@
+package com.javahub.codingBat;
+
+import java.util.Scanner;
+
+/**
+ * @author Raji on 22-03-2017.
+ */
+public class P01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("HelloWorld.");
+
+        System.out.println(inputString);
+
+    }
+}
